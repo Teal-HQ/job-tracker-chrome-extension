cc42c5644672f8204841ee1d58d0b1183eb67276
Jorge Sierra <georgesierra@gmail.com>
2020-08-03 16:59:41 -0300

    prod envs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,11 @@
-export const COMPANY_API_URL = 'https://teal-company-service-staging.herokuapp.com/';
-export const AUTH_API_URL = 'https://teal-auth-service-staging.herokuapp.com/';
-export const WEB_CLIENT_URL = 'http://workstyles-staging.tealhq.com/';
+export const COMPANY_API_URL = 'https://company.service.tealhq.com/';
+export const AUTH_API_URL = 'https://auth.service.tealhq.com/';
+export const WEB_CLIENT_URL = 'https://app.tealhq.com/';
+
+// TODO leaving these here for development
+// export const COMPANY_API_URL = 'https://teal-company-service-staging.herokuapp.com/';
+// export const AUTH_API_URL = 'https://teal-auth-service-staging.herokuapp.com/';
+// export const WEB_CLIENT_URL = 'http://workstyles-staging.tealhq.com/';
 
 export enum PAGES {
   ABOUT = 'about',
