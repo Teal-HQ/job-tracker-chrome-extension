5f18792fd3fcc604e63152f812d3729837fdc3b5
Cristina Silva <casilva62592@gmail.com>
2020-08-04 12:17:29 -0400

    Revert changes from local dev

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,9 +6,6 @@ export const WEB_CLIENT_URL = 'https://app.tealhq.com/';
 // export const COMPANY_API_URL = 'https://teal-company-service-staging.herokuapp.com/';
 // export const AUTH_API_URL = 'https://teal-auth-service-staging.herokuapp.com/';
 // export const WEB_CLIENT_URL = 'http://workstyles-staging.tealhq.com/';
-// export const COMPANY_API_URL = 'http://localhost:3003/';
-// export const AUTH_API_URL = 'http://localhost:3001/';
-// export const WEB_CLIENT_URL = 'http://workstyles-staging.tealhq.com/';
 
 export enum PAGES {
   ABOUT = 'about',
