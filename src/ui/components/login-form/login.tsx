98c0f61720a4da48d4332df7727c3415537c1656
Cristina Silva <casilva62592@gmail.com>
2020-08-03 22:31:29 -0400

    Update styles of login screen and add raleway font

--- src/ui/components/login-form/login.tsx
+++ src/ui/components/login-form/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Form, Button, Input, Alert } from 'antd';
+import { Row, Form, Button, Input, Alert } from 'antd';
 
 export interface ILoginForm {
   login: any,
@@ -9,46 +9,43 @@ export interface ILoginForm {
 const LoginForm = (props: ILoginForm) => {
   return (
     <div className="login-form">
+      <div className="login-header">
+        <img
+          className="teal-logo"
+          src={chrome.runtime.getURL('images/teal_logo_43.svg')}
+        />
+        <h3>Sign in to Teal</h3>
+      </div>
       <Row justify="center">
-        <Col>
-          <img
-            className="teal-logo"
-            src="https://app.tealhq.com/content/images/teal_logo_full_white.svg"
-          />
-        </Col>
-      </Row>
-      <Row justify="center">
-        <Col>
-          {props.error ? (
-            <Alert message={<div><strong>Failed to sign in!</strong> Please check your credentials or check your email to activate your account.</div>} type="error"/>
-          ) : null}
-          <Form
-            name="basic"
-            onFinish={props.login}
+        {props.error ? (
+          <Alert message={<div><strong>Failed to sign in!</strong> Please check your credentials or check your email to activate your account.</div>} type="error"/>
+        ) : null}
+        <Form
+          name="basic"
+          onFinish={props.login}
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: 'Please input your email!' }]}
           >
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[{ required: true, message: 'Please input your email!' }]}
-            >
-              <Input />
-            </Form.Item>
+            <Input />
+          </Form.Item>
 
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-              <Input.Password />
-            </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </Col>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </Row>
     </div>
   );
