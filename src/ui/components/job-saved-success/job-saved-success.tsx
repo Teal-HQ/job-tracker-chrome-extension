0ff1e56ef2915b6ce9eaa774d96077d9de33d92a
Keith Gould <keithmgould@gmail.com>
2020-08-10 15:45:21 -0400

    formatting cleanup

--- src/ui/components/job-saved-success/job-saved-success.tsx
+++ src/ui/components/job-saved-success/job-saved-success.tsx
@@ -21,9 +21,10 @@ const JobSavedSuccess = (props: IJobSavedSuccess) => {
             <img src={chrome.runtime.getURL('images/teal_logo_43.svg')}/>
           </Col>
           { 
-            data.id && <Col span={18}>
-            <div>{data.company} {data.role}</div>
-            <div>Job saved. <a target="_blank" href={`${WEB_CLIENT_URL}job-tracker/${data.id}`}>View now &#8594;</a></div>
+            data.id && 
+            <Col span={18}>
+              <div>{data.company} {data.role}</div>
+              <div>Job saved. <a target="_blank" href={`${WEB_CLIENT_URL}job-tracker/${data.id}`}>View now &#8594;</a></div>
             </Col>
           }
         </Row>
